69a4e5826230d0ef90958d2ffa3a27633c1d3a5e
hyperstown <experynt@gmail.com>
2022-05-07 12:41:07 +0200

    fix cards height for very large images

--- showImg.user.js
+++ showImg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ShowImageForReddit
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Images are always fully shown. No annoying see full image button!
 // @author       hyperstown
 // @match        https://www.reddit.com/*
@@ -11,28 +11,37 @@
     window.addEventListener('scroll', throttle(callback, 300));
 
     function callback() {
-        let buttons = document.getElementsByClassName('_3hUbl08LBz2mbXjy0iYhOS');
-        let wrapper1 = document.getElementsByClassName('_3Oa0THmZ3f5iZXAQ0hBJ0k');
-        let wrapper2 = document.getElementsByClassName('m3aNC6yp8RrNM_-a0rrfa');
+        let card = document.getElementsByClassName('_1oQyIsiPHYt6nx7VOmd1sz');
+        // _3-miAEojrCvx_4FQ8x3P-s
 
-        for(let i = 0; i < buttons.length; i++){
-            buttons[i].style.display = 'none';
-        }
+        for(let i = 0; i < card.length; i++){
+            let button = card[i].querySelector('._3hUbl08LBz2mbXjy0iYhOS')
+            if(button){
+                button.style.display = 'none'
+                let wrapper1 = card[i].querySelector('._3Oa0THmZ3f5iZXAQ0hBJ0k')
+                let wrapper2 = card[i].querySelector('.m3aNC6yp8RrNM_-a0rrfa');
+                let navButtons = card[i].querySelector('.STit0dLageRsa2yR4te_b')
 
-        for(let i = 0; i < wrapper1.length; i++){
-            wrapper1[i].style.maxHeight = "unset";
-        }
+                if(wrapper1){
+                    wrapper1.style.maxHeight = "unset";
+                }
 
-        for(let i = 0; i < wrapper2.length; i++){
-            let height = wrapper2[i].querySelector('img')?.height || null;
-            if(height){
-                wrapper2[i].style.maxHeight = height + "px";
-            }
-            else {
-                wrapper2[i].style.maxHeight = "unset";
+                if(wrapper2){
+                    let height = wrapper2.querySelector('img')?.height || null;
+                    if(height){
+                        wrapper2.style.maxHeight = height + "px";
+                        card[i].style.paddingRight = "40px";
+                        card[i].style.height = (height + 115) + "px";
+                        navButtons.style.height = height + "px";
+                    }
+                    else {
+                        wrapper2.style.maxHeight = "unset";
+                    }
+                }
             }
         }
     }
+    
 
     function throttle(fn, wait) {
         var time = Date.now();
